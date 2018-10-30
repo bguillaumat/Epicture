61517cc909c4b2885528c848c5de9410c4ee5a9f
bguillaumat <bastien.guillaumat@epitech.eu>
2018-10-30 15:27:08 +0100

    Add login page with webview

---
+++ app/src/main/java/brice_bastien/epicture/Main2Activity.java
@@ -0,0 +1,14 @@
+package brice_bastien.epicture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}

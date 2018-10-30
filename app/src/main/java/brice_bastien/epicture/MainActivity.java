c8fc7815640fc091f65836e2e500c899d4b907d6
BastienG <bastien.guillaumat@gmail.com>
2018-10-30 22:40:31 +0100

    Add read and write on phone

--- app/src/main/java/brice_bastien/epicture/MainActivity.java
+++ app/src/main/java/brice_bastien/epicture/MainActivity.java
@@ -1,5 +1,6 @@
 package brice_bastien.epicture;
 
+import android.content.Context;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -9,7 +10,15 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String Token = "";
+    private String Etc = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +35,46 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //Read data
+        try {
+            openData();
+        } catch (IOException ignored) {
+            onStop();
+        }
     }
 
+    @Override
+    protected void onPause() {
+        closeData();
+        super.onPause();
+    }
+
+    private void openData() throws IOException {
+        String FILENAME = "data";
+        String token = "";
+        String etc = "";
+        FileInputStream fos = openFileInput(FILENAME);
+        Scanner sc = new Scanner(fos);
+        if (sc.hasNextLine()) token = sc.nextLine();
+        if (sc.hasNextLine()) etc = sc.nextLine();
+        fos.close();
+        Token = token;
+        Etc = etc;
+    }
+
+    private void closeData() {
+        String FILENAME = "data";
+        String token = Token;
+        String etc = Etc;
+        String finalStr = (token + '\n' + etc + '\n');
+        try (FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE)) {
+            fos.write(finalStr.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.

0806da1e4a58b9aa21918a3bc6c3bee15bbc0d62
AsianPw <brice.lang-nguyen@epitech.eu>
2018-11-06 20:48:50 +0100

    Fix 2 call for startActivity(MainActivity)

--- app/src/main/java/brice_bastien/epicture/LoginActivity.java
+++ app/src/main/java/brice_bastien/epicture/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -23,25 +24,30 @@ public class LoginActivity extends AppCompatActivity {
 
 		final WebView webView = findViewById(R.id.webView);
 		webView.setWebViewClient(new WebViewClient() {
-									 @Override
-									 public void onPageStarted(WebView view, String url, Bitmap favicon) {
-										 String tmp = url.replace("#", "?");
-										 Uri uri = Uri.parse(tmp);
-										 Set<String> args = uri.getQueryParameterNames();
-										 Map<String, String> parameters = new HashMap<>();
-										 for (String s : args) {
-											 String s1 = uri.getQueryParameter(s);
-											 parameters.put(s, s1);
-										 }
-										 if (parameters.containsKey("access_token")) {
-											 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-											 intent.putExtra("ACCESS_TOKEN", parameters.get("access_token"));
-											 startActivity(intent);
-											 return;
-										 }
-										 super.onPageStarted(view, url, favicon);
-									 }
-								 });
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				if (url.startsWith("https://m.imgur.com/")) {
+					super.onPageStarted(view, url, favicon);
+					return;
+				}
+				Log.d("MyActivity", url);
+				String tmp = url.replace("#", "?");
+				Uri uri = Uri.parse(tmp);
+				Set<String> args = uri.getQueryParameterNames();
+				Map<String, String> parameters = new HashMap<>();
+				for (String s : args) {
+					String s1 = uri.getQueryParameter(s);
+					parameters.put(s, s1);
+				}
+				if (parameters.containsKey("access_token")) {
+					Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+					intent.putExtra("ACCESS_TOKEN", parameters.get("access_token"));
+					startActivity(intent);
+					return;
+				}
+				super.onPageStarted(view, url, favicon);
+			}
+		});
 		webView.getSettings().setJavaScriptEnabled(true);
 		webView.getSettings().setDomStorageEnabled(true);
 		webView.loadUrl("https://api.imgur.com/oauth2/authorize?client_id=8c94575ba123f37&response_type=token");

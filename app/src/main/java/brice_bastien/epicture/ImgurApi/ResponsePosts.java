3d9c471606cdf17000e5f0c4c156ccb61e7e9378
Brice Lang-nguyen <brice.lang-nguyen@epitech.eu>
2018-11-12 23:20:05 +0100

    Add ressources

---
+++ app/src/main/java/brice_bastien/epicture/ImgurApi/ResponsePosts.java
@@ -0,0 +1,28 @@
+package brice_bastien.epicture.ImgurApi;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+
+import org.json.JSONObject;
+
+public class ResponsePosts implements Response.Listener<JSONObject> {
+
+	private Context context;
+
+	public ResponsePosts(Context context) {
+		this.context = context;
+	}
+
+	@Override
+	public void onResponse(JSONObject response) {
+		try {
+			Log.i("GetData", response.toString(2));
+		} catch (Exception e) {
+			Log.i("ResponseJsonPosts", e.toString());
+			Toast.makeText(context, "Check our internet connection", Toast.LENGTH_SHORT).show();
+		}
+	}
+}
